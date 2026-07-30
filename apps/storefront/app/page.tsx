6e6c701c3218b7d6d7d09cc001a9619157fed112
yedeibj <yedei.bj@gmail.com>
2026-07-30 10:13:29 +0100

    Update page.tsx

--- apps/storefront/app/page.tsx
+++ apps/storefront/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   return (
     <main>
       <Header />
-      <Hero />
+      <HeroSlider />
       <CategoryGrid />
       <SubcategoryNav />
       <NewArrivals />
