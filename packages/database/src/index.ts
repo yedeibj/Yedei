4b8a5bcb3dd537b5c386b3c84c81c25f57b7109f
yedeibj <yedei.bj@gmail.com>
2026-07-31 00:59:13 +0100

    Delete packages/database/src/index.ts

--- packages/database/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { createClient as createBrowserSupabaseClient } from "./client";
-export { createClient as createServerSupabaseClient } from "./server";
-export { createMiddlewareClient } from "./middleware";
