13d5cb072bba68577d0a02359b0bdcba12d80ed3
yedeibj <yedei.bj@gmail.com>
2026-07-30 09:30:20 +0100

    Create PromoBar.tsx

---
+++ apps/storefront/components/PromoBar.tsx
@@ -0,0 +1,8 @@
+<span className="flex gap-[3px]" aria-hidden="true">
+-  <span className="h-[3px] w-[10px] rounded-full bg-[#2f6b4f]" />
+-  <span className="h-[3px] w-[7px] rounded-full bg-[#ad3b3b]" />
+-  <span className="h-[3px] w-[13px] rounded-full bg-[#233e6c]" />
++  <span className="h-[3px] w-[10px] rounded-full bg-[#006400]" />
++  <span className="h-[3px] w-[7px] rounded-full bg-[#dc143c]" />
++  <span className="h-[3px] w-[13px] rounded-full bg-[#00008b]" />
+</span>
