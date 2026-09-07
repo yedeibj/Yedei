eb68597fe1b86d2103eee7074c5b7630bc8f8cb0
yedeibj <yedei.bj@gmail.com>
2026-09-07 12:11:28 +0100

    Create ServiceWorkerRegister.tsx

---
+++ apps/storefront/components/ServiceWorkerRegister.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(() => {
+        // échec silencieux, pas critique
+      });
+    }
+  }, []);
+
+  return null;
+}
